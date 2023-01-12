c9682ae99b56a0940e300b25d806ac4dd222191d
Kyle Holmberg <kylemh.email@gmail.com>
2023-01-12 16:14:31 +0700

    integrate playwright and upload baseline

---
+++ e2e/landing-page.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveTitle(/Create Next App/);
+});
+
+test('renders correctly', async ({ page }) => {
+  await page.goto('/');
+  await expect(page).toHaveScreenshot('desktop/landing-page.png');
+});
